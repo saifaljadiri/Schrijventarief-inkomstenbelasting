05a27d26614ff0150fdbc429bfa56dadf578bba0
saifaljadiri <39760012+saifaljadiri@users.noreply.github.com>
2018-07-30 20:53:00 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace schrijventarief_inkomstenbelasting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(305, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tariefgroep 1",
+            "Tariefgroep 2",
+            "Tariefgroep 3",
+            "Tariefgroep 4",
+            "Tariefgroep 5",
+            "+12%"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 95);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(228, 37);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 82);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bereken";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(305, 381);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 35);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 381);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Te betalen belasting";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 756);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace schrijventarief_inkomstenbelasting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double inkomen = Convert.ToDouble(textBox1.Text);
+
+            double belastbaresom;
+            double belastvrijesom;
+            double tebetalenbelasting;
+
+            double tariefgroep1 = 419;
+            double tariefgroep2 = 8799;
+            double tariefgroep3 = 17179;
+            double tariefgroep4 = 15503;
+            double tariefgroep5 = 15503;
+
+            if (comboBox1.Items[Convert.ToInt32(comboBox1.SelectedIndex)].Equals(comboBox1.Items[0]))
+            {
+                belastvrijesom = tariefgroep1;
+            }
+            else if (comboBox1.Items[Convert.ToInt16(comboBox1.SelectedIndex)] == comboBox1.Items[1])
+            {
+                belastvrijesom = tariefgroep2;
+            }
+            else if (comboBox1.Items[Convert.ToInt16(comboBox1.SelectedIndex)] == comboBox1.Items[2])
+            {
+                belastvrijesom = tariefgroep3;
+            }
+            else if (comboBox1.Items[Convert.ToInt16(comboBox1.SelectedIndex)] == comboBox1.Items[3])
+            {
+                belastvrijesom = tariefgroep4;
+            }
+            else if (comboBox1.Items[Convert.ToInt16(comboBox1.SelectedIndex)] == comboBox1.Items[4])
+            {
+                belastvrijesom = tariefgroep5;
+            }
+            else if (comboBox1.Items[Convert.ToInt16(comboBox1.SelectedIndex)] == comboBox1.Items[5])
+            {
+                belastvrijesom = inkomen / 100 * 12;
+
+                if (belastvrijesom >= 6704)
+                {
+                    belastvrijesom = 6704;
+                }
+            }
+            else
+            {
+                belastvrijesom = 0;
+            }
+
+            belastbaresom = inkomen - belastvrijesom;
+
+            if (belastbaresom <= 8000)
+            {
+                tebetalenbelasting = belastbaresom / 100 * 35.75;
+            }
+            else if(belastbaresom >= 8001 && belastbaresom <= 25000)
+            {
+                tebetalenbelasting = belastbaresom / 100 * 37.05;
+
+            }
+            else if (belastbaresom >= 25001 && belastbaresom <= 54000)
+            {
+                tebetalenbelasting = belastbaresom / 100 * 50;
+
+            }
+            else
+            {
+                tebetalenbelasting = belastbaresom / 100 * 60;
+
+            }
+
+            textBox2.Text = Convert.ToString(tebetalenbelasting);
+
+        }
+    }
+}
